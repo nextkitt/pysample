c44699d7e1c9b18c7a00c409109c26f01de6ff1a
nextkitt <nextkitt@gmail.com>
2017-06-27 16:08:16 +0800

    init

---
+++ sample/core.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+def hello():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    hello()
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+from setuptools import setup
+
+setup(
+    name = "sample",
+    package="sample",
+    version = "0.0.1a",
+    url="http://github.com"
+)
